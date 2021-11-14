e8cb814d2a5f2788e6119237560c7fdd6adf75a9
nice-nicole <nicenicole.in@gmail.com>
2021-11-14 14:38:32 +0200

    changed width and height of the window

---
+++ sm_gol-main/game_window_class.py
@@ -0,0 +1,64 @@
+import pygame
+import copy
+from cell_class import *
+vec = pygame.math.Vector2
+
+class Game_window():
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.pos = vec(x, y)
+        self.width, self.height = 780, 680
+        self.image =pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rows = 30
+        self.cols = 30
+        self.grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+        for row in self.grid:
+            for cell in row:
+                cell.get_neighbours(self.grid)
+    def update(self):
+        self.rect.topleft = self.pos
+        for row in self.grid:
+            for cell in row:
+                cell.update()
+    def draw(self):
+        self.image.fill((102, 102, 102))
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+        self.screen.blit(self.image, (self.pos.x, self.pos.y))
+
+    def reset_grid(self):
+        self.grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+        for row in self.grid:
+            for cell in row:
+                cell.get_neighbours(self.grid)
+
+    def evaluate(self):
+        # for row in self.grid:
+        #     for cell in row:
+        #         for neighbour in cell.neighbours:
+        #             if neighbour.alive:
+        #                 cell.alive_neighbours += 1
+        # new_grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+        new_grid = copy.copy(self.grid)
+        for row in self.grid:
+            for cell in row:
+                cell.live_neighbours()
+
+        for yidx, row in enumerate(self.grid):
+            for xidx, cell in enumerate(row):
+                if cell.alive:
+                    if cell.alive_neighbours < 2:
+                        new_grid[yidx][xidx].alive = False
+                    if cell.alive_neighbours > 3:
+                        new_grid[yidx][xidx].alive = False
+                    if cell.alive_neighbours == 2 or cell.alive_neighbours == 3:
+                        new_grid[yidx][xidx].alive = True
+                else:
+                    if cell.alive_neighbours == 3:
+                        new_grid[yidx][xidx].alive = True
+        self.grid = new_grid
+
+
+
---
+++ sm_gol-main/main.py
@@ -0,0 +1,165 @@
+import builtins
+import pygame
+import sys
+
+from pygame import mouse 
+from game_window_class import *
+from button_class import *
+
+width, height = 800, 800
+background = (199, 199, 199)
+FPS = 60
+
+
+def get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_on_grid(mouse_pos):
+                click_cell(mouse_pos)
+            else:
+                for button in buttons:
+                    button.click()
+
+
+def update():
+    game_window.update()
+    for button in buttons:
+        # button.update()
+        button.update(mouse_pos, game_state=state)
+
+def draw():
+    window.fill(background)
+    for button in buttons:
+        button.draw()
+    game_window.draw()
+
+# functions for the running functionality
+
+def running_get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_on_grid(mouse_pos):
+                click_cell(mouse_pos)
+            else:
+                for button in buttons:
+                    button.click()
+
+
+def running_update():
+    game_window.update()
+    for button in buttons:
+        # button.update()
+        button.update(mouse_pos, game_state=state)
+    if frame_count%(FPS//10) == 0:
+        game_window.evaluate()
+
+def running_draw():
+    window.fill(background)
+    for button in buttons:
+        button.draw()
+    game_window.draw()
+
+# function for the pausing functionality
+
+def paused_get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_on_grid(mouse_pos):
+                click_cell(mouse_pos)
+            else:
+                for button in buttons:
+                    button.click()
+
+
+def paused_update():
+    game_window.update()
+    for button in buttons:
+        # button.update()
+        button.update(mouse_pos, game_state=state)
+
+def paused_draw():
+    window.fill(background)
+    for button in buttons:
+        button.draw()
+    game_window.draw()
+
+# end of the particular pausing functions
+
+#once there is a mouse on a grid, this lines of codes from 26 to 37 should make a cell alive   
+def mouse_on_grid(pos):
+    if pos[0] > 100 and pos[0] < width-20:
+        if pos[1] > 100 and pos[1] < height - 20:
+            return True
+        return False
+
+def click_cell(pos):
+    grid_pos = [pos[0]-180, pos[1]-100]
+    grid_pos[0] = grid_pos[0]//20
+    grid_pos[1] = grid_pos[1]//20
+    #this if statement allow to make a cell alive or dead by just clicking on it
+    if game_window.grid[grid_pos[1]][grid_pos[0]].alive:
+        game_window.grid[grid_pos[1]][grid_pos[0]].alive = False
+    else:
+        game_window.grid[grid_pos[1]][grid_pos[0]].alive = True
+
+
+def make_buttons():
+    buttons = []
+    buttons.append(Button(window, width//2-50, 50, 100, 20, text = 'RUN', colour = (28, 111, 51), hover_colour = (92, 232, 77), function=run_game, state='setting'))
+    buttons.append(Button(window, width//2-50, 50, 100, 20, text = 'PAUSE', colour = (18, 104, 135), hover_colour = (140, 180, 245), function= pause_game, state='running'))
+    buttons.append(Button(window, width//2-50, 50, 100, 20, text = 'RESET', colour = (117, 14, 14), hover_colour = (237, 66, 75), function = reset_grid, state='paused'))
+    return buttons
+
+def run_game():
+    global state
+    state = 'running'
+
+def pause_game():
+    global state
+    state = 'paused'
+
+def reset_grid():
+    pass
+
+pygame.init()
+window = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+game_window = Game_window(window, 180, 100)
+buttons = make_buttons()
+state = 'setting'
+frame_count = 0
+
+
+running = True
+while running:
+    frame_count += 1
+    mouse_pos = pygame.mouse.get_pos()
+    if state == 'setting':
+        get_events() 
+        update()
+        draw()
+    if state == 'running':
+        running_get_events() 
+        running_update()
+        running_draw()
+    if state == 'paused':
+        paused_get_events() 
+        paused_update()
+        paused_draw()
+    pygame.display.update()
+    clock.tick(FPS)
+    print(state)
+pygame.quit()
+sys.exit()
